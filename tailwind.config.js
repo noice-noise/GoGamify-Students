29f840a7a8d8b34e8b2517c3a0d2713f26126625
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-04-25 17:57:04 +0800

    build: add tailwindcss tools and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}", "./views/**/*.{html,js,ejs}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["'Poppins'", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
+    plugins: [],
+  },
+};
