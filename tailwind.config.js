ce6c76943adbecf9b51de16864e9ac2277d69c25
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-04-26 21:55:18 +0800

    styles(themes): add default theme pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
       fontFamily: {
         poppins: ["'Poppins'", "Helvetica", "Arial", "sans-serif"],
       },
+      colors: {
+        primary: withOpacityValue("--color-primary"),
+        "primary-accent": withOpacityValue("--color-primary-accent"),
+        "primary-muted": withOpacityValue("--color-primary-muted"),
+        neutral: withOpacityValue("--color-text-neutral"),
+        foreground: withOpacityValue("--color-foreground"),
+        background: withOpacityValue("--color-background"),
+        success: withOpacityValue("--color-success"),
+        danger: withOpacityValue("--color-danger"),
+        warning: withOpacityValue("--color-warning"),
+      },
     },
     plugins: [],
   },
 };
+
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`;
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`;
+  };
+}
