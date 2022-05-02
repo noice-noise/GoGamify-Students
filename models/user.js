9c9d8e8aeff3a9cff78ced2b8daf9b2b00a22be8
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-05-02 09:28:18 +0800

    chore: add user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
