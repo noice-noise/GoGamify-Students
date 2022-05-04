26ee6a1b55955652188fa20880422f8498442705
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-05-04 18:10:40 +0800

    feat: add additional authentication for first time login

--- config/authConfig.js
+++ config/authConfig.js
@@ -9,7 +9,16 @@ module.exports = {
 
   forwardAuthenticated: function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-      return res.redirect("/home.html");
+      return res.redirect("/home");
+    }
+
+    return next();
+  },
+
+  forwardFirstLogin: function checkFirstLogin(req, res, next) {
+    console.log("First time login");
+    if (req.user.profile === undefined || req.user.profile == "NA") {
+      return res.redirect("/get-started");
     }
 
     return next();
