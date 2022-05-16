58a5ff6d635e37cdf243d5218f9e5bfef842639f
junix033101 <64710006+junix033101@users.noreply.github.com>
2022-05-17 03:06:41 +0800

    -added more dummy cards and content (todo tasks) -added more dummy cards and content (completed tasks) -fixed show more button colors on hover -added explore button -1 dynamic progress bar

--- public/pwa/journey/journey.js
+++ public/pwa/journey/journey.js
@@ -1,3 +1,5 @@
+const { doc } = require("prettier");
+
 document.getElementById("btn_comp").click = openComp();
 document.getElementById("btn_todo").click = openTodo();
 document.getElementById("btn_show").click = show(id);
@@ -5,24 +7,27 @@ document.getElementById("btn_show").click = show(id);
 
 
 function openComp(){
- document.getElementById("todo").style.display = "none";
- document.getElementById("todo2").style.display = "none";
- document.getElementById("todo3").style.display = "none";
- document.getElementById("todo4").style.display = "none";
- document.getElementById("todo1").style.display = "flex";
+    for(var x = 1; x<9; x++){
+        document.getElementById("todo"+x).style.display = "none"; 
+    }
+    for(var x = 11; x<15; x++){
+        document.getElementById("todo"+x).style.display = "flex"; 
+    }
  document.getElementById("btn_comp").style.backgroundColor = 'rgb(var(--color-primary)';
  document.getElementById("btn_todo").style.backgroundColor = 'rgb(var(--color-primary-muted) / var(--tw-bg-opacity))';
-
+ console.log("COMPLETE BUTTON: CLICKED!");
 
 }
 function openTodo(){
-    document.getElementById("todo").style.display = "flex";
-    document.getElementById("todo2").style.display = "flex";
-    document.getElementById("todo3").style.display = "flex";
-    document.getElementById("todo4").style.display = "flex";
-    document.getElementById("todo1").style.display = "none";
+    for(var x = 1; x<9; x++){
+        document.getElementById("todo"+x).style.display = "flex"; 
+    }    for(var x = 11; x<15; x++){
+        document.getElementById("todo"+x).style.display = "none"; 
+    }
+
     document.getElementById("btn_todo").style.backgroundColor = 'rgb(var(--color-primary)';
     document.getElementById("btn_comp").style.backgroundColor = 'rgb(var(--color-primary-muted) / var(--tw-bg-opacity))';
+    console.log("TODO BUTTON: CLICKED!");
 
 }
 
@@ -32,29 +37,28 @@ function show(id){
     var new_txt = document.getElementById("txt_show"+id);
     var icon_more = document.getElementById("icon_more"+id);
     var icon_less = document.getElementById("icon_less"+id);
-    const svg = document.querySelector(".btn_exp");
     if(x.style.display == "none"){
         x=  x.style.display = "block";
-        new_txt = new_txt.innerHTML = "show less";
         icon_less = icon_less.style.visibility = "visible";
         icon_more = icon_more.style.visibility = "hidden";
-        btn.focus();
-        btn.hover();
-        svg.classList.add('focused');
-        svg.classList.remove('unfocused');
+        // btn.hover();
+        // svg.classList.add('focused');
+        // svg.classList.remove('unfocused');
+        new_txt = new_txt.innerHTML = "show less";
+        // btn.focus();
+        console.log("SHOW MORE BUTTON: CLICKED");
 
 
     } 
     else{
         x.style.display="none";
-        new_txt.innerHTML="show more";
         icon_more = icon_more.style.visibility = "visible";
         icon_less = icon_less.style.visibility = "hidden";
-        svg.classList.add('unfocused');
-        svg.classList.remove('focused');
-        btn.blur();
-
+        // svg.classList.remove('focused');
+        // svg.classList.add('unfocused');
+        new_txt.innerHTML="show more";
+        // btn.blur();
+        console.log("SHOW LESS BUTTON: CLICKED");
     }
-
-
 }
+
