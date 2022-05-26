27821cb6b4bfbb78568d5a2df66911ba6ce56fd3
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-05-26 22:45:03 +0800

    feat: add pwa dynamic routes

--- routes/pwaRoutes.js
+++ routes/pwaRoutes.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const pwaController = require("../controllers/pwaController");
+router.get("/journey/completed", (req, res) => {
+  res.render("app/journey-done", {
+    title: "Journey Completed | GoGamify",
+  });
+});
 
-// router.get("/", pwaController.pwa_index);
-// router.post("/profile/theme", pwaController.theme_post);
+router.get("/journey", (req, res) => {
+  res.redirect("/pwa/journey/journey.html");
+});
+
+router.get("/collections", (req, res) => {
+  res.redirect("/pwa/collections/collections.html");
+});
+
+router.get("/community", (req, res) => {
+  res.redirect("/pwa/community/community.html");
+});
+
+router.get("/module", (req, res) => {
+  res.redirect("/pwa/learning-module/module.html");
+});
+
+router.get("/profile", (req, res) => {
+  res.redirect("/pwa/profile/profile.html;");
+});
 
 module.exports = router;
