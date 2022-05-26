75faf00bb0c3813bcbdcf374378ff53c93bf92d4
junix033101 <64710006+junix033101@users.noreply.github.com>
2022-05-26 23:12:20 +0800

    journey-changed file name

--- public/pwa/journey/journey.js
+++ public/pwa/journey/journey.js
@@ -1,64 +1,80 @@
-const { doc } = require("prettier");
-
-document.getElementById("btn_comp").click = openComp();
-document.getElementById("btn_todo").click = openTodo();
-document.getElementById("btn_show").click = show(id);
 
 
+// fetch('https://gogamify-education.herokuapp.com/student/p/resources/')
+//     .then(response =>response.json())
+//     .then(responseJson=>console.log(responseJson));
 
-function openComp(){
-    for(var x = 1; x<9; x++){
-        document.getElementById("todo"+x).style.display = "none"; 
-    }
-    for(var x = 11; x<15; x++){
-        document.getElementById("todo"+x).style.display = "flex"; 
-    }
- document.getElementById("btn_comp").style.backgroundColor = 'rgb(var(--color-primary)';
- document.getElementById("btn_todo").style.backgroundColor = 'rgb(var(--color-primary-muted) / var(--tw-bg-opacity))';
- console.log("COMPLETE BUTTON: CLICKED!");
-
-}
-function openTodo(){
-    for(var x = 1; x<9; x++){
-        document.getElementById("todo"+x).style.display = "flex"; 
-    }    for(var x = 11; x<15; x++){
-        document.getElementById("todo"+x).style.display = "none"; 
+const data = [
+    {
+        title: "English week 1",
+        subtitle: "Reading and fuck Writing",
+    },
+    {
+        title: "planet paper"
+    },
+    {
+        title: "modern history"
+    },
+    {
+        title: "fuck artssdf"
     }
 
-    document.getElementById("btn_todo").style.backgroundColor = 'rgb(var(--color-primary)';
-    document.getElementById("btn_comp").style.backgroundColor = 'rgb(var(--color-primary-muted) / var(--tw-bg-opacity))';
-    console.log("TODO BUTTON: CLICKED!");
-
-}
-
-function show(id){
-    var btn = document.getElementById("btn_show");
-    var x = document.getElementById("more"+id);
-    var new_txt = document.getElementById("txt_show"+id);
-    var icon_more = document.getElementById("icon_more"+id);
-    var icon_less = document.getElementById("icon_less"+id);
-    if(x.style.display == "none"){
-        x=  x.style.display = "block";
-        icon_less = icon_less.style.visibility = "visible";
-        icon_more = icon_more.style.visibility = "hidden";
-        // btn.hover();
-        // svg.classList.add('focused');
-        // svg.classList.remove('unfocused');
-        new_txt = new_txt.innerHTML = "show less";
-        // btn.focus();
-        console.log("SHOW MORE BUTTON: CLICKED");
+];
 
 
-    } 
-    else{
-        x.style.display="none";
-        icon_more = icon_more.style.visibility = "visible";
-        icon_less = icon_less.style.visibility = "hidden";
-        // svg.classList.remove('focused');
-        // svg.classList.add('unfocused');
-        new_txt.innerHTML="show more";
-        // btn.blur();
-        console.log("SHOW LESS BUTTON: CLICKED");
+let getInitials = function (string) {
+    let names = string.split(' '),
+        initials = names[0].substring(0, 1).toUpperCase();
+    
+    if (names.length > 1) {
+        initials += names[1].substring(0, 1).toUpperCase();
     }
-}
+    return initials;
+};
 
+
+
+data.forEach((element, i) => {
+
+    const container = document.querySelector(".file-container");
+    //create
+
+    const card = document.createElement('div');
+    card.classList = 'card-main-container';
+
+    const cardContent = `                
+        <div class="subject-box">
+            <p class="h1 text-box">${getInitials(data[i].title)}</p>
+        </div>
+        <div class = "card-body">
+            <p class="h3">${data[i].title}</p>
+            <p class="h4 sub-title">${data[i].subtitle}</p>
+            <div>
+                <div class="meter yellow">
+                    <span style='width: 60%'>
+                    </span>
+                </div>
+                <div class="progress-text">
+                    <p class="h6">Progress</p>
+                    <div>
+                        <!--progress width length basis-->
+                        <span class="h6" id="current">10</span>
+                        <span class="h6"> / </span>
+                        <span class="h6 total" id="total">20</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" class="btn_exp" height="32" width="32"
+            viewBox="0 0 50 50">
+            <path d="M24 30.75 12 18.75 14.15 16.6 24 26.5 33.85 16.65 36 18.8Z"
+                class="svg_more" id="icon_more1" />
+            <path d="M14.15 30.75 12 28.6 24 16.6 36 28.55 33.85 30.7 24 20.85Z"
+                class="svg_less" id="icon_less1">
+        </svg>
+        </a>`;
+    
+    card.innerHTML += cardContent;
+    container.appendChild(card);
+});
