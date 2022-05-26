44d382ea48815d8e1d833ad707aea83440565260
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-05-26 19:08:39 +0800

    chore: add ensure authentication

--- index.js
+++ index.js
@@ -118,8 +118,8 @@ app.use("/get-started", (req, res) => {
 app.use("/pwa", pwaRoutes);
 app.use("/gamify", gamifyRoutes);
 app.use("/resource", learningResourceRoutes);
-// app.use("/student", ensureAuthenticated, studentRoutes);
-// app.use("/collection", ensureAuthenticated, collectionRoutes);
+app.use("/student", ensureAuthenticated, studentRoutes);
+app.use("/collection", ensureAuthenticated, collectionRoutes);
 app.use("/student", studentRoutes);
 app.use("/collection", collectionRoutes);
 
