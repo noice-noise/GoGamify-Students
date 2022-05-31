b40caac880389dba51e56e238ef6316b9aff0e01
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2022-05-31 10:43:03 +0800

    Integrated the code for Badges
    The generate_achievements now support badges depending on the section parent's(param) id
    
    Also able to generate the element with an img, title, and description.

--- public/pwa/collections/collections.js
+++ public/pwa/collections/collections.js
@@ -1,4 +1,4 @@
-console.log('Debug');
+
 const user_url = "/student/profile"
 var fullname2 = ""
 const preferences = JSON.parse(window.localStorage.getItem("preferences"));
@@ -22,49 +22,48 @@ function getUserCredentials() {
 
 /* Coded on May 30 2022: I just hope na makagenerate siyag child badges ug child achievements */
 const achievement_section = document.getElementById('achievement');//parent
-
-//Implicit
-// const achievement_child1 = document.createElement("div");
-// const achievement_child2 = document.createElement("div");
-// const achievement_child3 = document.createElement("div");
-// const achievement_child4 = document.createElement("div");
-
-// achievement_child1.classList.add('card');
-// achievement_child1.classList.add('card--extra-small');
-// achievement_child1.classList.add('text-center');
-
-// achievement_child2.classList.add('card__container');
-// achievement_child3.classList.add('card__header');
-// achievement_child4.classList.add('card__img-container');
-
-// achievement_child3.appendChild(achievement_child4);
-// achievement_child2.appendChild(achievement_child3);
-// achievement_child1.appendChild(achievement_child2);
-// achievement_section.appendChild(achievement_child1);
+const badges_section = document.getElementById('badge');
+// console.log(achievement_section.id) console.log(badges_section.id)
 
 //Explicit
-generateAchievements();
-generateAchievements();
+generateAchievements(achievement_section);
+generateAchievements(badges_section);
 
-function generateAchievements() {
+function generateAchievements(section_parent) {
     const achievement_child1 = document.createElement("div");
     const achievement_child2 = document.createElement("div");
     const achievement_child3 = document.createElement("div");
     const achievement_child4 = document.createElement("div");
 
-    achievement_child1.classList.add('card');
-    achievement_child1.classList.add('card--extra-small');
-    achievement_child1.classList.add('text-center');
-
+    achievement_child1.classList.add('card','card--extra-small','text-center');
     achievement_child2.classList.add('card__container');
     achievement_child3.classList.add('card__header');
     achievement_child4.classList.add('card__img-container');
 
+    var achievement_img = document.createElement('img');
+    var achievement_header = document.createElement('p')
+    var achievement_desc = document.createElement('p')
+    achievement_img.classList.add('card__img','mx-auto','h-12','w-12');
+    achievement_img.src = './badges/Begginer.png';
+    achievement_img.alt = 'Profile Image';
+    achievement_header.classList.add('h2','text-primary');
+    achievement_header.innerHTML = 'Baby Steps(gen)'
+    achievement_desc.classList.add('text-primary-accent');
+    achievement_desc.innerHTML = 'Read 1 Learning Module on any subject'
+
+    achievement_child4.appendChild(achievement_img);
+    achievement_child4.appendChild(achievement_header);
+    achievement_child4.appendChild(achievement_desc);
+
     achievement_child3.appendChild(achievement_child4);
     achievement_child2.appendChild(achievement_child3);
     achievement_child1.appendChild(achievement_child2);
-    achievement_section.appendChild(achievement_child1);
+    if(section_parent.id = 'achievement'){
+        section_parent.appendChild(achievement_child1);
+    }else{
+        section_parent.appendChild(achievement_child1);
+    }
+    
 }
 
-console.log(achievement_section);
 
