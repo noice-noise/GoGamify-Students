1087261675356dade85c82e30c7618588b73d134
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-06 23:55:54 +0800

    feat: allow browsing of journey inside students PWA

--- routes/pwaRoutes.js
+++ routes/pwaRoutes.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const LearningResource = require("../models/learningResource.js");
+
 router.get("/journey/completed", (req, res) => {
   res.render("app/journey-done", {
     title: "Journey Completed | GoGamify",
   });
+});
+
+router.get("/journey/browse", (req, res) => {
+  console.log("Retrieving learning resources from DB...");
+  LearningResource.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      console.log("Number of Learning Resources: ", result.length);
+      res.render("app/browse-journey", {
+        title: "Explore Journey | GoGamify",
+        resources: result,
+        offline: false,
+      });
+    })
+    .catch((err) => {
+      res.render("404", { title: "Sorry, something went wrong." });
+    });
 });
 
 router.get("/journey", (req, res) => {
