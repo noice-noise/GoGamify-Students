42018c694bd4e5db8769a0a404f1bb514ed69190
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-10 17:13:39 +0800

    build: remove Poppins as default sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Poppins"', "Helvetica", "Arial", "sans-serif"],
+        sans: ["Helvetica", "Arial", "sans-serif"],
       },
       colors: {
         primary: withOpacityValue("--color-primary"),
