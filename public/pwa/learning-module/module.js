9e347967868db2cb4673563ceaad55d577fcde08
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-10 16:55:13 +0800

    chore(module): dynamic state of page nav buttons

--- public/pwa/learning-module/module.js
+++ public/pwa/learning-module/module.js
@@ -4,6 +4,12 @@ const moduleRoot = document.getElementById("moduleRoot");
 const title = document.getElementById("title");
 const subtitle = document.getElementById("subtitle");
 const pageInfo = document.getElementById("pageInfo");
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let module;
+
+backBtn.addEventListener("click", (e) => {});
 
 const initScript = async () => {
   const preferences = await JSON.parse(
@@ -37,12 +43,35 @@ const fetchModule = async () => {
     })
     .then((data) => {
       console.log("data", data);
+      module = data;
       appendHeader(data.header);
       appendContent(data.body);
+      handlePageNav();
     })
     .catch((err) => {
       console.log(err);
     });
+};
+
+const handlePageNav = () => {
+  console.log("handlepagenav");
+  if (!module) {
+    console.log("Error, module not defined.");
+  }
+
+  if (module.header.currentPageNumber <= 0) {
+    console.log("hide back");
+    backBtn.classList.add("hidden");
+  } else {
+    backBtn.classList.remove("hidden");
+  }
+
+  if (module.header.currentPageNumber == module.header.pages - 1) {
+    console.log("show completed");
+    nextBtn.textContent = "Complete Journey";
+  } else {
+    nextBtn.textContent = "Next";
+  }
 };
 
 const appendHeader = (data) => {
