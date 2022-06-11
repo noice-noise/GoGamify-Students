b5e604e21116de9b960b7423938a12416892344f
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-11 17:03:55 +0800

    fix: remove enrolled students array to allow addToSet queries

--- models/learningResource.js
+++ models/learningResource.js
@@ -1,37 +1,33 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const learningResourceSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    subtitle: {
-      type: String,
-      required: false,
-    },
-    owner: {
-      type: String,
-      required: true,
-    },
-    ownerId: {
-      type: String,
-      required: true,
-    },
-    body: {
-      type: Object,
-      required: true,
-    },
-    students: [],
-    modules: [],
-    pages: { type: Number },
-    collectibles: {
-      type: Array,
-    },
+const learningResourceSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  subtitle: {
+    type: String,
+    required: false,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  ownerId: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: Object,
+    required: true,
+  },
+  modules: [],
+  pages: { type: Number },
+  collectibles: {
+    type: Array,
+  },
+});
 
 const LearningResource = mongoose.model(
   "LearningResource",
