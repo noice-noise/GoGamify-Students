647953bca77f13bd840180966d63a0e1db581fdc
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-12 18:07:26 +0800

    chore: add timestamps again

--- models/learningResource.js
+++ models/learningResource.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const learningResourceSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const learningResourceSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+    },
+    owner: {
+      type: String,
+      required: true,
+    },
+    ownerId: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: Object,
+      required: true,
+    },
+    modules: [],
+    pages: { type: Number },
+    collectibles: {
+      type: Array,
+    },
   },
-  subtitle: {
-    type: String,
-    required: false,
-  },
-  owner: {
-    type: String,
-    required: true,
-  },
-  ownerId: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: Object,
-    required: true,
-  },
-  modules: [],
-  pages: { type: Number },
-  collectibles: {
-    type: Array,
-  },
-});
+  { timestamps: true }
+);
 
 const LearningResource = mongoose.model(
   "LearningResource",
